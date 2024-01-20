aedf2f2d877e241e5104cc8001ea839c06764b7c
hakan-yigit <147969806+hakan-yigit@users.noreply.github.com>
2024-01-20 12:38:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Star Wars</h1>
+      <p>
+        <strong>
+          Developer Olmak En Az Star Wars Serisi Kadar Uzun Ve Yorucudur..
+        </strong>
+      </p>
+      <img src="https://i.pinimg.com/564x/45/3c/20/453c20bd22dc37b8847e5ab419bcd1d6.jpg"></img>
+      <p>
+        <strong>
+          Bazen Ödevleri Anlamak Gerçek Anakin Skywalker'ı bulmak kadar zordur.
+        </strong>
+      </p>
+    </div>
+  );
+}
